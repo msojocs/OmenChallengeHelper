ba125b281d1996522540e9680b9babe30d53fe23
jiye <jiyecafe@gmail.com>
2021-05-27 18:38:27 +0800

    feat: 自动化支持。

--- src/main/java/org/example/Utils/HTTP/HttpUtil.java
+++ src/main/java/org/example/Utils/HTTP/HttpUtil.java
@@ -96,7 +96,7 @@ public class HttpUtil {
         final RequestConfig config = unBuildConfig
                 .setConnectTimeout(Timeout.ofSeconds(5))
                 .setRedirectsEnabled(false)
-                .setProxy(new HttpHost("127.0.0.1", 8866))      // TODO:开发环境设置代理
+                // .setProxy(new HttpHost("127.0.0.1", 8866))      // TODO:开发环境设置代理
                 .setCircularRedirectsAllowed(true)
                 .build();
 
